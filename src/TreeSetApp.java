33422f8058cc6171c83987923ef97e63e4db9162
Sukma Anggarmadi <2111527001_sukma@student.unand.ac.id>
2022-12-07 20:11:31 +0700

    baru

--- src/TreeSetApp.java
+++ src/TreeSetApp.java
@@ -1,9 +1,10 @@
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class TreeSetApp {
   public static void main(String[] args) {
-    Set<Integer> nilai = new TreeSet<>();
+    SortedSet<Integer> nilai = new TreeSet<>();
 
     nilai.add(87);
     nilai.add(89);
@@ -12,9 +13,20 @@ public class TreeSetApp {
     nilai.add(76);
     nilai.add(55);
     nilai.add(37);
-    nilai.add(0);
+    nilai.add(10);
     nilai.add(70);
 
+    var a = nilai.subSet(10, 70);
     System.out.println(nilai);
+    System.out.println(a);
+    /*
+     * Method yang bisa untuk Sorted Set
+     * comparator() -> untuk membandingkan 2 data
+     * subSet(E,F) -> mengambil nilai lebih besar atau sama dengan E dan kecil dari F
+     * headSet(E) -> mengambil nilai yang kecil atau sama dengan E
+     * tailSet(E) -> mengambil nilai yang lebih besar dari E
+     * first(E) -> Melihat data pertama setelah di urutkan
+     * last(E) -> Melihat data terakhir setelah di urutkan
+     */
   }
 }
